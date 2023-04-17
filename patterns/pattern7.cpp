e6c3591f770623ad315c3c4013172927daacba6d
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-17 23:57:11 +0530

    implemented p7

--- patterns/pattern7.cpp
+++ patterns/pattern7.cpp
@@ -14,18 +14,18 @@ int main()
     int n;
     cin >> n;
 
-    // i jytine bar chalega, j bhi utna bar hi chalega
-    // much time i will run, that much time j will also run, so
+    // 'row' jytine bar chalega, 'col' bhi utna bar hi chalega
+    // much time 'row' will run, that much time 'col' will also run, so
     // 1 row, 1 colunms
     // 2 row, 2 colunms
     // 3 row, 3 colunms
     // n row, n colunms
 
-    for (int i = 1; i <= n; i++)
+    for (int row = 1; row <= n; row++)
     {
-        for (int j = 1; j <= i; j++) // 1, 1 --> 2, 2 --> 3, 3
+        for (int col = 1; col <= row; col++) // 1, 1 --> 2, 2 --> 3, 3
         {
-            cout << i;
+            cout << row;
         }
         cout << endl;
     }
