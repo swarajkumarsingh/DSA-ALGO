3dfb4734d244ba81e17400d5b23758a0a9f7310b
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-18 00:04:42 +0530

    p8

---
+++ patterns/pattern8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// Pattern
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+
+int main()
+{
+
+    int n, count = 1;
+    cin >> n;
+
+    // 'row' jytine bar chalega, 'col' bhi utna bar hi chalega
+    // much time 'row' will run, that much time 'col' will also run, so
+    // 1 row, 1 colunms
+    // 2 row, 2 colunms
+    // 3 row, 3 colunms
+    // n row, n colunms
+
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++) // 1, 1 --> 2, 2 --> 3, 3
+        {
+            cout << count << " ";
+        }
+        count = count + 1;
+        cout << endl;
+    }
+    return 0;
+}
