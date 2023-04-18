65ca18fc90911e48bc5bf1619c9235aaf4ff5376
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-18 13:02:39 +0530

    p11

---
+++ patterns/pattern11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// Pattern
+// A
+// A A
+// B B B
+// C C C C
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            char ch = 'A' + i;
+            cout << ch;
+        }
+        cout << endl;
+    }
+    return 0;
+}
