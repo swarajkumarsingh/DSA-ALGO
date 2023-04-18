95be0d6c2159c780134c126469a46a4bd2d11849
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-18 13:21:50 +0530

    p12

---
+++ patterns/pattern12.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// Pattern
+// A B C
+// D E F
+// G H I
+
+int main()
+{
+    int n;
+    char ch = 'A';
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << ch << " ";
+            ch = ch + 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
