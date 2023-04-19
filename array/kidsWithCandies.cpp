2053db0a603fa88697889358d4c04138ce4aa65c
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-19 23:30:22 +0530

    kidsWithCandies question solved :)

--- array/kidsWithCandies.cpp
+++ array/kidsWithCandies.cpp
@@ -6,11 +6,35 @@ using namespace std;
 
 vector<bool> kidsWithCandies(vector<int> candies, int extraCandies)
 {
+    int max_candies = *max_element(candies.begin(), candies.end());
+
+    vector<bool> result(candies.size());
+
+    for (int i = 0; i < candies.size(); i++)
+    {
+        if (candies[i] + extraCandies >= max_candies)
+        {
+            result[i] = true;
+        }
+        else
+        {
+            result[i] = false;
+        }
+    }
+
+    for (int j = 0; j < result.size(); j++)
+    {
+        cout << result[j] << " ";
+    }
+    cout << endl;
+}
+
+vector<bool> kidsWithCandies2(vector<int> candies, int extraCandies)
+{
     vector<bool> result = {};
     for (int i = 0; i < candies.size(); i++)
     {
         double max = *max_element(candies.begin(), candies.end());
-        cout << max << endl;
         if (candies[i] + extraCandies >= max)
         {
             result.push_back(true);
@@ -18,7 +42,6 @@ vector<bool> kidsWithCandies(vector<int> candies, int extraCandies)
         else
         {
             result.push_back(false);
-            candies[0] = candies[i];
         }
     }
 
@@ -30,8 +53,11 @@ vector<bool> kidsWithCandies(vector<int> candies, int extraCandies)
 
 int main()
 {
+    vector<int> candies = {12, 1, 12};
+    int extraCandies = 10;
 
-    kidsWithCandies({2, 3, 5, 1, 3}, 3);
+    kidsWithCandies(candies, extraCandies);
+    kidsWithCandies2(candies, extraCandies);
 
     return 0;
 }
