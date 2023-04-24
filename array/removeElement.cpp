3744e161a5a873f207804f123d662b59f806ab37
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-25 00:08:41 +0530

    removeElement sample code.

--- array/removeElement.cpp
+++ array/removeElement.cpp
@@ -3,10 +3,21 @@
 
 using namespace std;
 
+vector<int> removeElement(vector<int> nums, int ele)
+{
+    vector<int> newNums = {};
 
+    for (int i = 0; i < nums.size(); i++)
+    {
+        
+    }
+    
+
+    return newNums;
+}
 
 int main()
 {
-    // removeElement({3, 2, 2, 3}, 3);
+    removeElement({1, 2}, 2);
     return 0;
 }
