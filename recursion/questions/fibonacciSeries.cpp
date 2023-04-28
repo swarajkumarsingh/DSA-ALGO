eefe58669fe730eeb0cc94f4906ce1800d414338
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-29 00:55:42 +0530

    implemented fibonacciSeries.cpp

---
+++ recursion/questions/fibonacciSeries.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return func(n - 2) + func(n - 1);
+}
+
+int main()
+{
+    cout << func(7);
+    return 0;
+}
