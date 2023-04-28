c1785709082df7eab5516712802054427a83e0b3
swarajkumarsingh <sswaraj169@gmail.com>
2023-04-28 14:40:02 +0530

    spell check done on powerOfNumber.cpp

--- recursion/types/headRecursion.cpp
+++ recursion/types/headRecursion.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // Here the first statement inside the function is recursive call and
 //  all the processing of this function will be done after that( in returning time )
 
-// Code below the function will execute, in the returning time of the funvtion
+// Code below the function will execute, in the returning time of the function
 
 void fun(int n)
 {
