45522e1a2d1c8b0a4bf4a30ce44c9056de86346b
swarajkumarsingh <sswaraj169@gmail.com>
2023-05-07 16:03:56 +0530

    coded for fibonacciSeries.cpp

--- recursion/questions/fibonacciSeries.cpp
+++ recursion/questions/fibonacciSeries.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int func(int n)
-{
-    if (n <= 1)
-    {
-        return n;
-    }
-    return func(n - 2) + func(n - 1);
-}
-
-int func2(int n)
 {
     if (n <= 1)
     {
