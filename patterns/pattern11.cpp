53c18ee1e027dd8c992787936c1b2874416799a1
swarajkumarsingh <sswaraj169@gmail.com>
2023-05-10 19:15:00 +0530

    re-implemented pattern11.cpp

--- patterns/pattern11.cpp
+++ patterns/pattern11.cpp
@@ -15,9 +15,9 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= i; j++)
         {
-            char ch = 'A' + i;
+            char ch = 'A' + i - 1;
             cout << ch;
         }
         cout << endl;
