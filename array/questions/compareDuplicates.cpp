47918ea68bcf99101a7d18ceeacb26267f6ad672
swarajkumarsingh <sswaraj169@gmail.com>
2023-05-14 21:50:37 +0530

    added comments in compareDuplicates.cpp

--- array/questions/compareDuplicates.cpp
+++ array/questions/compareDuplicates.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+// Steps
+//? > Here we are using unordered_set, coz we will use o(n) (single for loop) so we need a list which does not contain duplicated values
+// 1. Loop thorugh every element for(int num: nums) {...}
+// 2. check is the element is already present in the unique set
+// 3. Also push element in the set
+// 4. repeat the process
+
 bool containsDuplicate(vector<int> &nums)
 {
     unordered_set<int> mySet;
