0e9a3963080d5a83a6a896e8872c7ab3d2354d7a
swarajkumarsingh <sswaraj169@gmail.com>
2023-05-15 23:46:59 +0530

    added binarySearchRecursion in binarySearch.cpp

--- Sorting/binarySearch.cpp
+++ Sorting/binarySearch.cpp
@@ -24,9 +24,28 @@ int binarySearch(vector<int> nums, int target)
     return -1;
 }
 
+int binarySearchRecursion(vector<int> nums, int l, int h, int target)
+{
+    int mid;
+    sort(nums.begin(), nums.end());
+
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+            return binarySearchRecursion(nums, l, mid - 1, target);
+        else
+            return binarySearchRecursion(nums, mid + 1, h, target);
+    }
+    return -1;
+}
+
 int main()
 {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int _ = binarySearch(nums, 9);
-    cout << _;
+    int __ = binarySearchRecursion(nums, 0, nums.size(), 3);
+    cout << _ << __;
 }
