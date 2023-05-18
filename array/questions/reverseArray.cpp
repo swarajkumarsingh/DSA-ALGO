d313511f1e27f020d1ff18ed88a18db46e330c7d
Swaraj Kumar Singh <sswaraj169@gmail.com>
2023-05-18 14:46:20 +0530

    added comments on reverseArray.cpp

--- array/questions/reverseArray.cpp
+++ array/questions/reverseArray.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+//! Reverse an Array
+// 1,2,3,4,5,6 --> 6,5,4,3,2,1
+// Okay so here we are swapping the last corresponding values, i is starting from first and j is staring from the last, and first element is equal to last element
+// Lis this :-
+// We are swapping the first and last elements
+// 1 2 3 4          1 2 3 4
+// |     |            | |
+// |     |            | |
+// |-----|            |-|
+
 vector<int> reverseArray(vector<int> nums)
 {
     int temp;
