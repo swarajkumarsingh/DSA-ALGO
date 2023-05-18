6f86475047fbc8bc9759c8b0325e693262a6fddb
Swaraj Kumar Singh <sswaraj169@gmail.com>
2023-05-18 14:46:00 +0530

    added comments on leftShift.cpp

--- array/questions/leftShift.cpp
+++ array/questions/leftShift.cpp
@@ -3,17 +3,23 @@
 
 using namespace std;
 
+//! Left Shifting Array 
+// 1,2,3,4 --> 2,3,4,0
+// okay, so here nums[i] is equal to nums[i+1](the next element), and if i == size-1(last element in the array) then replace it with 0
+
 vector<int> leftShift(vector<int> nums)
 {
-
     int size = nums.size();
     for (int i = 0; i < size; i++)
     {
+        // Replace last element with 0
         if (i == size - 1)
         {
             nums[i] = 0;
             return nums;
         }
+
+        // Change the values
         nums[i] = nums[i + 1];
     }
 
