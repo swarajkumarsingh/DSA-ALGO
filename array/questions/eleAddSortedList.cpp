c8af026f2abe67262b16aa9644c54af59467f77f
Swaraj Kumar Singh <sswaraj169@gmail.com>
2023-05-18 18:17:10 +0530

    completed eleAddSortedList.cpp

---
+++ array/questions/eleAddSortedList.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> addElementInSortedList(vector<int> nums, int x)
+{
+    int i = nums.size() - 1;
+    // 8 > 7
+    while (nums[i] > x)
+    {
+        nums[i + 1] = nums[i];
+        i--;
+    }
+    nums[i + 1] = x;
+
+    return nums;
+}
+
+vector<int> addElementInSortedList1()
+{
+    vector<int> A = {1, 2, 3, 4, 5, 6, 8, 9, 10};
+    int x = 7;
+    int i = A.size() - 1;
+
+    while (A[i] > x)
+    {
+        A[i + 1] = A[i];
+        i--;
+    }
+    A[i + 1] = x;
+
+    // Display
+    for (int i = 0; i < A.size(); i++)
+    {
+        cout << A[i] << " ";
+    }
+}
+
+int main()
+{
+    addElementInSortedList1();
+    return 0;
+}
