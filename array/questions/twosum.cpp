f3e0804333c2a8e40882ae4a291f092f0cadd3c3
Swaraj Kumar Singh <sswaraj169@gmail.com>
2023-05-18 15:09:53 +0530

    added comments on twoSum.cpp

--- array/questions/twosum.cpp
+++ array/questions/twosum.cpp
@@ -3,23 +3,24 @@
 
 using namespace std;
 
-void twoSum(vector<int> &nums, int target)
+// {1,2} target = 3 --> 0,1
+// {1,2,4,5,6} target = 11 --> 3,4
+
+vector<int> twoSum(vector<int> &nums, int target)
 {
+    // Loop through every element and it's surroundings
     for (int i = 0; i < nums.size(); i++)
     {
         for (int j = 0; j < nums.size(); j++)
         {
+            // ignore adding if i==j --> 0,0 --> 1,1 --> 2,2
+            if (i == j) continue;
 
-            if (i == j)
-            {
-                continue;
-            }
-
+            // Check if nums[i] + nums[j] == target then return the indexes values
             if (nums[i] + nums[j] == target)
             {
                 cout << i << j << endl;
-                // return {i, j};
-                return;
+                return {i, j};
             }
         }
     }
