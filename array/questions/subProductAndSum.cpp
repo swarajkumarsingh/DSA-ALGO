afc0cca93cec75be8f5fb7e8a96e389d5e4138e4
Swaraj Kumar Singh <sswaraj169@gmail.com>
2023-05-25 02:44:21 +0530

    added comments to subProductAndSum.cpp

--- array/questions/subProductAndSum.cpp
+++ array/questions/subProductAndSum.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
-// 234 --> 15
+/*
+Question: 234, take the number, and perform multiplication and addition on every digit of the number, and at the last subtract - multi minus add
+
+Answer:
+Step 1: while(n!=0)
+Step 2: int digit = n % 10; get the last digit of the number (234) --> 4
+Step 3: prod *= digit; multiply every digit of the number
+Step 4: sum += digit; add every digit of the number
+Step 5: n = n / 10; reduce the number of digits (234) -> 23; (23) -> 3; until the number if 0
+Step 6: repeat the process
+Step 7: int res = prod - sum; find the res which i prod - sum;
+*/
 
 int subProductAndSum(int n)
 {
