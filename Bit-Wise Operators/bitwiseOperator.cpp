20c20c1dc385720ada38360d3c350568ad972827
swarajkumarsingh <sswaraj169@gmail.com>
2023-06-03 17:14:37 +0530

    added decimal notes and coded bitwise Operator.cpp

---
+++ Bit-Wise Operators/bitwiseOperator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Bit Wise Operators are used for operations related to bits(01110101...)
+Types of bitwise operators :-
+    1. And --> &
+    2. OR --> |
+    3. Not --> ~
+    4. XOR --> ^
+*/
+
+int main()
+{
+
+    int a = 4, b = 6;
+    cout << "HELLO";
+    // cout << "a&b" << a & b;
+    // cout << "a|b" << a | b;
+    // cout << "~a" << ~a;
+    // cout << "a^b" << a ^ b;
+
+    return 0;
+}
