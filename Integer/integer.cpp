9b4f853b6a989dca7e3dc6b94db1a92be258440b
swarajkumarsingh <sswaraj169@gmail.com>
2023-06-08 16:01:33 +0530

    added Integer folder, with integer.cpp

---
+++ Integer/integer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int n = 123;
+
+    // Get last number of the digit
+    int lastDigit = n % 10;
+
+    // Remove the last number 
+    int new_n = n / 10;
+
+    // check if the number is in the range of integer
+    if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)){
+        // ... code 
+    }
+
+    return 0;
+}
