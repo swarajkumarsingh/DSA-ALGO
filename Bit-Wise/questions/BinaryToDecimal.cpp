f10a1a38db6ebfefa796bd84c62551c1ec1cec5c
swarajkumarsingh <sswaraj169@gmail.com>
2023-06-08 16:18:52 +0530

    re-implemented binaryToDecimal.cpp

--- Bit-Wise/questions/BinaryToDecimal.cpp
+++ Bit-Wise/questions/BinaryToDecimal.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <math.h>
-
 using namespace std;
-
-// Binary to Decimal
-/*
-1. Get the last digit of the number
-2. if the last digit is 1, then add pow(2, i) in ans
-3. Remove the lastDigit, so nextTime, another digit appears
-*/
 
 int main()
 {
@@ -20,22 +12,17 @@ int main()
 
     while (n != 0)
     {
-        // Get last digit 123 --> 3
-        int lastDigit = n % 10;
 
-        // if last digit is 1; then follow the 2 pow series
-        if (lastDigit == 1)
+        int digit = n % 10;
+
+        if (digit == 1)
         {
             ans = ans + pow(2, i);
         }
 
-        // remove the last digit, so that next time, the second last digit comes 123 --> 12 --> 1
         n = n / 10;
         i++;
     }
-
-    cout << "Binary: " << n << " | | "
-         << "Decimal: " << ans << endl;
-
+    cout << ans << endl;
     return 0;
 }
