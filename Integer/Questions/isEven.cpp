3eb2edd62cb0c32b732f2e75563b2e0504d3b032
swaraj <sswaraj169@gmail.com>
2023-06-23 23:04:32 +0530

    added isEven.cpp

---
+++ Integer/Questions/isEven.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void isEven(int num)
+{
+    if (num % 2 == 0)
+    {
+        cout << "EVEN Number";
+    }
+    else
+    {
+        cout << "ODD Number";
+    }
+}
+
+int main()
+{
+    int _;
+    cin >> _;
+    isEven(_);
+
+    return 0;
+}
--- Integer/integer.cpp
+++ Integer/integer.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <limits.h>
 
-int main(){
+int main()
+{
     int n = 123;
 
     // Get last number of the digit
     int lastDigit = n % 10;
 
-    // Remove the last number 
+    // Remove the last number
     int new_n = n / 10;
 
     // check if the number is in the range of integer
-    if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)){
-        // ... code 
+    if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10))
+    {
+        // ... code
     }
 
     return 0;
