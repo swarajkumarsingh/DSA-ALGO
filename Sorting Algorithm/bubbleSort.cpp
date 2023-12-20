d1516264336e3afdd5dd8f7fd159075419de79e9
swaraj <sswaraj169@gmail.com>
2023-12-20 14:56:22 +0530

    implemented: optimized bubbleSort.cpp

--- Sorting Algorithm/bubbleSort.cpp
+++ Sorting Algorithm/bubbleSort.cpp
@@ -9,13 +9,16 @@ vector<int> bubbleSort(vector<int> arr, int n)
 {
     for (int i = 1; i < n; i++)
     {
+        bool isSwap = false;
         for (int j = 0; j < n - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                isSwap = true;
             }
         }
+        if (!isSwap) break;
     }
 
     return arr;
