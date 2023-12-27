a5121316e110596d155b11c090de00c955a10abb
swaraj <sswaraj169@gmail.com>
2023-12-27 21:21:26 +0530

    implemented: reverseArray.cpp

--- array/questions/reverseArray.cpp
+++ array/questions/reverseArray.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //! Reverse an Array
 // 1,2,3,4,5,6 --> 6,5,4,3,2,1
 // Okay so here we are swapping the last corresponding values, i is starting from first and j is staring from the last, and first element is equal to last element
-// Lis this :-
+// Like this :-
 // We are swapping the first and last elements
 // 1 2 3 4          1 2 3 4
 // |     |            | |
@@ -43,6 +43,21 @@ vector<int> reverseArray1(vector<int> nums)
             nums[start] = nums[end];
             nums[end] = temp;
         }
+    }
+
+    return nums;
+}
+
+vector<int> reverseArray2(vector<int> nums)
+{
+    int s = 0;
+    int e = nums.size() - 1;
+
+    while (s<=e)
+    {
+        swap(nums[s], nums[e]);
+        s++;
+        end--;
     }
 
     return nums;
